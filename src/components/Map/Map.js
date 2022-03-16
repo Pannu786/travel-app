cbee03a7bade5ce11bcc3f82e55b5b60e5472f00
Pannu786 <thepannu786@gmail.com>
2022-03-16 19:10:56 +0530

    Added rating cmp into placedetails/map cmp

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,7 @@
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
@@ -32,7 +32,7 @@ export const Map = ({ coordinates, setCoordinates, setBounds, places }) => {
             lng={+place.longitude}
             key={i}
           >
-            { isDesktop ? (
+            {isDesktop ? (
               <LocationOnOutlinedIcon color='primary' fontSize='large' />
             ) : (
               <Paper elevation={3} className={classes.paper}>
@@ -42,12 +42,17 @@ export const Map = ({ coordinates, setCoordinates, setBounds, places }) => {
                   gutterBottom
                 >
                   {place.name}
-                  </Typography>
-                  <img
-                    className={classes.pointer}
-                    src={place.photo
-                    ? place.photo.images.large.url
-                    : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'} alt={place.name}/>
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+                  }
+                  alt={place.name}
+                />
+                <Rating size='small' value={+place.rating} readyOnly />
               </Paper>
             )}
           </div>
--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -33,6 +33,12 @@ export const PlaceDetails = ({ place }) => {
           {place.name}
         </Typography>
         <Box display='flex' justifyContent='space-between'>
+          <Rating value={+place.rating} readyOnly />
+          <Typography gutterBottom variant='subtitle1'>
+            Out of {place.num_reviews} reviews
+          </Typography>
+        </Box>
+        <Box display='flex' justifyContent='space-between'>
           <Typography variant='subtitle1'>Price</Typography>
           <Typography gutterBottom variant='subtitle1'>
             {place.price_level}
